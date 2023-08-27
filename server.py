8246d8f8c8ce0d06f9c6bb3ce5e1dee60c520d31
Shripad Rao <shripada0987@gmail.com>
2023-08-27 22:00:35 +0530

    add calc logic

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    resp = data['first'] + data['second']
+    return jsonify(resp)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    resp = data['first'] - data['second']
+    return jsonify(resp)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
